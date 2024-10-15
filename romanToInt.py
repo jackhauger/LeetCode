cc382ff7a1053085c4c301b3075ecc4180c4cd10
Jack Hauger <jackhauger5@gmail.com>
2024-10-14 23:09:07 -0400

    solution added

--- romanToInt.py
+++ romanToInt.py
@@ -1,3 +1,27 @@
-class Solution:
+class Solution2:
     def romanToInt(self, s: str) -> int:
+        stack = []
+        dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        total = 0
+
+        total = 0
+        prevValue = 0
+
+        for char in reversed(s):
+            currValue = dict[char]
+
+            if currValue < prevValue:
+                total -= currValue
+            else:
+                total += currValue
+
+            prevValue = currValue
+
+        return total
+
+solution = Solution2()
+
+z = solution.romanToInt('MCDLXXVI')
+print(z)
+
 
